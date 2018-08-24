7de15b13be64cd4ef6ec0d2e4493c62bbf62b514
Engr.Sunday <asmelitus@gmail.com>
2018-08-24 15:50:00 +0100

    created babelrc file and installed some packeges

---
+++ src/server/index.jsx
@@ -0,0 +1,18 @@
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+
+const app = express();
+
+const schema = ...
+const resolvers = ...
+
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers,
+});
+
+server.applyMiddleware({ app, path: '/graphql' });
+
+app.listen({ port: 8000 }, () => {
+  console.log('Apollo Server on http://localhost:8000/graphql');
+});
